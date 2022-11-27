f8128ca72a047abbda7cff863be7f12c56f75079
harireddy7 <harimsd8@gmail.com>
2022-11-27 21:05:07 +0530

    Added compile by categories to loop multiple categories

---
+++ categories.js
@@ -0,0 +1,67 @@
+module.exports = [
+    {
+		label: 'Web Development',
+		link: '/topic/web-development/',
+        items: 7,
+	},
+    {
+        label: 'HTML',
+        link: '/topic/html/',
+        items: 5,
+    },
+	{
+		label: 'CSS',
+		link: '/topic/css/',
+        items: 4,
+	},
+	{
+		label: 'JavaScript',
+		link: '/topic/javascript/',
+        items: 5,
+	},
+    {
+		label: 'Node.Js',
+		link: '/topic/nodejs/',
+        items: 5,
+	},
+	{
+		label: 'MERN Stack',
+		link: '/topic/mern-stack/',
+        items: 7,
+	},
+	{
+		label: 'React Native',
+		link: '/topic/react-native/',
+        items: 4,
+	},
+    {
+		label: 'Redux Framework',
+		link: '/topic/redux-framework/',
+        items: 3,
+	},
+	{
+		label: 'Front End Web Development',
+		link: '/topic/front-end-web-development/',
+        items: 6,
+	},
+	{
+		label: 'Typescript',
+		link: '/topic/typescript/',
+        items: 4,
+	},
+	{
+		label: 'Full Stack Web Development',
+		link: '/topic/full-stack-web-development/',
+        items: 7,
+	},
+	{
+		label: 'MongoDB',
+		link: '/topic/mongodb/',
+        items: 3,
+	},
+	{
+		label: 'Angular',
+		link: '/topic/angular/',
+        items: 3,
+	},
+];
---
+++ compileByCategories.js
@@ -0,0 +1,123 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const { executablePath } = require('puppeteer');
+const { Cluster } = require('puppeteer-cluster');
+
+const fs = require('fs');
+const path = require('path');
+
+puppeteer.use(StealthPlugin());
+
+const CATEGORIES = require('./categories');
+
+const appendToFile = async (data, action) => {
+	const filePath = path.join(__dirname, 'data', 'courses.json');
+	if (action === 'create') {
+		// create file and write to it
+		fs.writeFileSync(filePath, JSON.stringify([]), {
+			encoding: 'utf-8',
+		});
+		const content = fs.readFileSync(filePath);
+		const newData = [...JSON.parse(content), ...data];
+
+		fs.writeFileSync(filePath, JSON.stringify(newData), {
+			encoding: 'utf-8',
+		});
+	} else {
+		// append data to existing file
+		const content = fs.readFileSync(filePath);
+		const newData = [...JSON.parse(content), ...data];
+
+		fs.writeFileSync(filePath, JSON.stringify(newData), {
+			encoding: 'utf-8',
+		});
+	}
+};
+
+const writeToFile = async (data) => {
+	const filePath = path.join(__dirname, 'data', 'courses.json');
+	const fileData = fs.readFileSync(filePath, { encoding: 'utf-8' });
+	
+	const content = fileData ? [...JSON.parse(fileData), ...data] : [...data];
+
+	fs.writeFileSync(filePath, JSON.stringify(content), {
+		encoding: 'utf-8',
+	});
+};
+
+const getAction = (category) => {
+	const isFirstCategory = CATEGORIES[0]?.label === category?.label;
+	return isFirstCategory ? 'create' : 'write';
+};
+
+(async () => {
+	const cluster = await Cluster.launch({
+		concurrency: Cluster.CONCURRENCY_PAGE,
+		maxConcurrency: 100,
+		monitor: true,
+		timeout: 120000,
+		puppeteerOptions: {
+			headless: false,
+			defaultViewport: false,
+		},
+	});
+
+	cluster.on('taskerror', (err, data) => {
+		console.log(`Error crawling ${JSON.stringify(data)}: ${err.message}`);
+	});
+
+	await cluster.task(async ({ page, data }) => {
+		await page.setViewport({
+			width: 1400,
+			height: 800,
+			deviceScaleFactor: 1,
+		});
+		await page.goto(`https://www.udemy.com${data.link}`, {
+			waitUntil: 'networkidle0',
+			timeout: 0,
+		});
+
+		await page.waitForSelector('[class^="course-list--container"]');
+
+		const courses = await page.evaluate((data) => {
+			const childNodes = [
+				...document.querySelector('[class^="course-list--container"]').childNodes,
+			];
+			// alert(childNodes.length);
+			const _courses = childNodes.reduce((list, node) => {
+				const c = {
+					title: node.getElementsByTagName('a')[0]?.innerHTML.split('<div')[0],
+					category: data.label,
+					description: node?.getElementsByTagName('p')[0]?.innerHTML,
+					instructor: node.querySelectorAll('[class^="course-card--instructor"]')[0]?.innerText,
+					rating: node.querySelectorAll('[data-purpose="rating-number"]')[0]?.innerText,
+					reviews: node.querySelectorAll('[class*="course-card--reviews-text"]')[0]?.innerText,
+					watchTime: node.querySelectorAll('[class*="course-card--course-meta-info"] span')[0]
+						?.innerText,
+					lectures: node.querySelectorAll('[class*="course-card--course-meta-info"] span')[1]
+						?.innerText,
+					level: node.querySelectorAll('[class*="course-card--course-meta-info"] span')[2]
+						?.innerText,
+					price: node.querySelectorAll('[data-purpose="course-price-text"] > span > span')[0]
+						?.innerText,
+				};
+				if (c.title?.includes('<span>') || !c.description || !c.instructor || !c.lectures) {
+					return list;
+				}
+				list.push(c);
+				return list;
+			}, []);
+
+			return _courses.slice(0, data.items);
+		}, data);
+
+		await writeToFile(courses);
+	});
+
+	for (const cat of CATEGORIES) {
+		await cluster.queue(cat);
+	}
+
+	await cluster.idle();
+	await cluster.close();
+})();
